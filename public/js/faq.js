bb4efce21641fcff3c49c70c135178673a01a3da
Harun <harunozb@gmail.com>
2024-03-27 21:46:56 +0300

    delete public

--- public/js/faq.js
+++
@@ -1,6 +0,0 @@
-var questions = document.querySelectorAll(".questions");
-questions.forEach((question) => {
-  question.addEventListener("click", ()=>{
-    question.classList.toggle("active");
-  })
-})
--- public/js/stone.js
+++
@@ -1,6 +0,0 @@
-var stones = document.querySelectorAll(".stone");
-stones.forEach((stone) => {
-  stone.addEventListener("click", ()=>{
-    stone.classList.toggle("active");
-  })
-})
